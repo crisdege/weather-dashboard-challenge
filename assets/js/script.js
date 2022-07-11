71cde13ef85b80d08c252c5417b6410e66cae5fa
Cristina Dege <cris.dege0813@gmail.com>
2022-07-11 10:28:19 -0400

    form submit button working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,14 @@
-var getLocationCoordinates = function () {
-  fetch(
-    "http://api.openweathermap.org/geo/1.0/direct?q=London&appid=f675d7e4c5368e20a20492b4a37fbb0c"
-  ).then(function (response) {
+var citySearchEl = document.querySelector("#city-search");
+var cityNameEl = document.querySelector("#city-name");
+
+// citySearchEl.addEventListener("click", formSubmitHandler);
+
+var getLocationCoordinates = function (name) {
+  var apiUrl =
+    "http://api.openweathermap.org/geo/1.0/direct?q=" +
+    name +
+    "&appid=f675d7e4c5368e20a20492b4a37fbb0c";
+  fetch(apiUrl).then(function (response) {
     response.json().then(function (data) {
       console.log(data);
     });
@@ -17,5 +24,19 @@ var getLocationForecast = function () {
     });
   });
 };
-getLocationCoordinates();
-getLocationForecast();
+
+var formSubmitHandler = function (event) {
+  event.preventDefault();
+  var cityname = cityNameEl.value.trim();
+
+  if (cityname) {
+    getLocationCoordinates(cityname);
+    cityNameEl.value = "";
+  } else {
+    alert("Please enter a City Name");
+  }
+};
+// getLocationCoordinates();
+// getLocationForecast();
+
+citySearchEl.addEventListener("submit", formSubmitHandler);
